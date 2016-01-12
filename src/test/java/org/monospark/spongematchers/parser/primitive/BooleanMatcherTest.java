25c7a359755fc98975d2d250b03069dcff24a553
Christopher Schnick <c.schnick@monospark.org>
2016-01-13 00:14:12 +0100

    Remove some useless unit tests

--- src/test/java/org/monospark/spongematchers/parser/primitive/BooleanMatcherTest.java
+++ src/test/java/org/monospark/spongematchers/parser/primitive/BooleanMatcherTest.java
@@ -18,39 +18,21 @@ public class BooleanMatcherTest {
     public void parseMatcher_True_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
         String input = "true";
         
-        verifyMatcher(input, false, true);
+        Optional<SpongeMatcher<Boolean>> matcher = SpongeMatcherParser.BOOLEAN.parseMatcher(input);
+        
+        assertTrue(matcher.isPresent());
+        assertThat(matcher.get(), matches(true));
+        assertThat(matcher.get(), not(matches(false)));
     }
     
     @Test
     public void parseMatcher_False_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
         String input = "false";
         
-        verifyMatcher(input, true, false);
-    }
-    
-    @Test
-    public void parseMatcher_TrueAndFalse_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
-        String input1 = "true|false";
-        String input2 = " true  | \t false ";
-        
-        verifyMatcher(input1, true, true);
-        verifyMatcher(input2, true, true);
-    }
-    
-    @Test
-    public void parseMatcher_FalseAndTrue_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
-        String input1 = "false|true";
-        String input2 = " false  | \t true ";
-        
-        verifyMatcher(input1, true, true);
-        verifyMatcher(input2, true, true);
-    }
-    
-    private void verifyMatcher(String input, boolean matchFalse, boolean matchTrue) throws SpongeMatcherParseException {
         Optional<SpongeMatcher<Boolean>> matcher = SpongeMatcherParser.BOOLEAN.parseMatcher(input);
         
         assertTrue(matcher.isPresent());
-        assertThat(matcher.get(), matchFalse ? matches(false) : not(matches(false)));
-        assertThat(matcher.get(), matchTrue ? matches(true) : not(matches(true)));
+        assertThat(matcher.get(), matches(false));
+        assertThat(matcher.get(), not(matches(true)));
     }
 }
