142188bfd6c552152ce2ea2cc16e74b6e8a7c90b
Christopher Schnick <c.schnick@monospark.org>
2016-01-23 11:20:21 +0100

    Add a unit test for parsing logical conjunctions

---
+++ src/test/java/org/monospark/spongematchers/parser/type/MatcherTypeTest.java
@@ -0,0 +1,23 @@
+package org.monospark.spongematchers.parser.type;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.monospark.spongematchers.testutil.HamcrestSpongeMatchers.matches;
+
+import org.junit.Test;
+import org.monospark.spongematchers.matcher.SpongeMatcher;
+import org.monospark.spongematchers.parser.SpongeMatcherParseException;
+import org.monospark.spongematchers.parser.element.StringElementParser;
+
+public class MatcherTypeTest {
+
+    @Test
+    public void parseMatcher_MatcherWithConjunctions_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
+        String input = "(('test1'|'test2')&!('test2'))";
+        
+        SpongeMatcher<String> matcher = MatcherType.STRING.parseMatcher(StringElementParser.parseStringElement(input));
+        
+        assertThat(matcher, matches("test1"));
+        assertThat(matcher, not(matches("test2")));
+    }
+}
