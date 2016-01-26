4f2deb4c14c806eb74329c6622347fadfda71a07
Christopher Schnick <c.schnick@monospark.org>
2016-01-26 13:19:40 +0100

    Rework property matchers

---
+++ src/main/java/org/monospark/spongematchers/matcher/sponge/PropertyHolderMatcher.java
@@ -0,0 +1,55 @@
+package org.monospark.spongematchers.matcher.sponge;
+
+import java.util.Map;
+
+import org.monospark.spongematchers.matcher.SpongeMatcher;
+import org.monospark.spongematchers.matcher.complex.MapMatcher;
+import org.spongepowered.api.data.property.PropertyHolder;
+
+public final class PropertyHolderMatcher extends SpongeObjectMatcher<PropertyHolder> {
+
+    public static SpongeMatcher<PropertyHolder> create(SpongeMatcher<Map<String, Object>> matcher) {
+        return new PropertyHolderMatcher(matcher);
+    }
+    
+    private PropertyHolderMatcher(SpongeMatcher<Map<String, Object>> matcher) {
+        super(matcher);
+    }
+
+    @Override
+    protected void fillMap(PropertyHolder o, Map<String, Object> map) {
+        o.getApplicableProperties().stream().forEach(p -> map.put(p.getClass().getName(), p.getValue()));
+    }
+       
+    public static Builder builder() {
+        return new Builder();
+    }
+    
+    public static final class Builder {
+        
+        private MapMatcher.Builder builder;
+        
+        private Builder() {
+            builder = MapMatcher.builder();
+        }
+        
+        public Builder addBooleanProperty(String name, SpongeMatcher<Boolean> matcher) {
+            builder.addMatcher(name, Boolean.class, matcher);
+            return this;
+        }
+        
+        public Builder addIntegerProperty(String name, SpongeMatcher<Long> matcher) {
+            builder.addMatcher(name, Long.class, matcher);
+            return this;
+        }
+        
+        public Builder addFloatingPointProperty(String name, SpongeMatcher<Double> matcher) {
+            builder.addMatcher(name, Double.class, matcher);
+            return this;
+        }
+        
+        public SpongeMatcher<PropertyHolder> build() {
+            return new PropertyHolderMatcher(builder.build());
+        }
+    }
+}
