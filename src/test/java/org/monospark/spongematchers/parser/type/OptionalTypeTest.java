90af5800d6054922a7c41bece7667273dc9f21f3
Christopher Schnick <c.schnick@monospark.org>
2016-01-27 14:16:28 +0100

    Add unit tests for the OptionalType class

---
+++ src/test/java/org/monospark/spongematchers/parser/type/OptionalTypeTest.java
@@ -0,0 +1,56 @@
+package org.monospark.spongematchers.parser.type;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.monospark.spongematchers.testutil.HamcrestSpongeMatchers.matches;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.monospark.spongematchers.matcher.SpongeMatcher;
+import org.monospark.spongematchers.parser.SpongeMatcherParseException;
+import org.monospark.spongematchers.parser.element.StringElement;
+import org.monospark.spongematchers.parser.element.StringElementParser;
+
+public class OptionalTypeTest {
+
+    @Test
+    public void canParse_DeepAndDifferentElementType_ReturnsFalse() throws SpongeMatcherParseException {
+        StringElement element = StringElementParser.parseStringElement("1");
+        
+        boolean canParse = MatcherType.optional(MatcherType.BOOLEAN).canParse(element, true);
+        
+        assertThat(canParse, is(false));
+    }
+    
+    @Test
+    public void canParse_NotDeepAndAnyElement_ReturnsTrue() throws SpongeMatcherParseException {
+        StringElement element = StringElementParser.parseStringElement("1");
+        
+        boolean canParse = MatcherType.optional(MatcherType.BOOLEAN).canParse(element, false);
+        
+        assertThat(canParse, is(true));
+    }
+    
+    @Test
+    public void parseMatcher_EmptyElement_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
+        StringElement element = StringElementParser.parseStringElement("empty");
+        
+        SpongeMatcher<Optional<Boolean>> matcher = MatcherType.optional(MatcherType.BOOLEAN).parseMatcher(element);
+        
+        assertThat(matcher, matches(Optional.empty()));
+        assertThat(matcher, not(matches(Optional.of(false))));
+    }
+    
+    @Test
+    public void parseMatcher_ValueTypeElement_ReturnsCorrectMatcher() throws SpongeMatcherParseException {
+        StringElement element = StringElementParser.parseStringElement("true");
+        
+        SpongeMatcher<Optional<Boolean>> matcher = MatcherType.optional(MatcherType.BOOLEAN).parseMatcher(element);
+        
+        assertThat(matcher, matches(Optional.of(true)));
+        assertThat(matcher, not(matches(Optional.of(false))));
+        assertThat(matcher, not(matches(Optional.empty())));
+    }
+}
